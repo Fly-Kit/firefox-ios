92ecef6e613d67054426fcc4d2d1cd223a913b74
isabelrios <isabelrios@gmail.com>
2018-05-25 12:42:35 +0200

    Bug 1463105 - New Integration test History entry

--- XCUITests/IntegrationTests.swift
+++ XCUITests/IntegrationTests.swift
@@ -22,6 +22,56 @@ class IntegrationTests: BaseTestCase {
         navigator.nowAt(BrowserTab)
     }
 
+    func testFxASyncTabs () {
+        navigator.openURL("www.slack.com")
+        waitUntilPageLoad()
+        navigator.goto(BrowserTabMenu)
+        navigator.goto(FxASigninScreen)
+        waitforExistence(app.webViews.staticTexts["Sign in"], timeout: 10)
+        userState.fxaUsername = ProcessInfo.processInfo.environment["FXA_EMAIL"]!
+        userState.fxaPassword = ProcessInfo.processInfo.environment["FXA_PASSWORD"]!
+        navigator.performAction(Action.FxATypeEmail)
+        navigator.performAction(Action.FxATypePassword)
+        navigator.performAction(Action.FxATapOnSignInButton)
+        allowNotifications()
+
+        // Wait for initial sync to complete
+        navigator.nowAt(BrowserTab)
+        navigator.goto(SettingsScreen)
+        waitforExistence(app.tables.staticTexts["Sync Now"], timeout: 10)
+    }
+
+    func testFxASyncHistory () {
+        // Go to a webpage, and add to bookmarks
+        navigator.createNewTab()
+        waitforExistence(app.textFields["url"])
+        app.textFields["url"].tap()
+        app.textFields["address"].typeText("www.slack.com")
+        app.typeText("\r")
+        waitUntilPageLoad()
+
+        // Go to History List from Browser context menu and there should be one entry
+        //waitforExistence(app.tables.cells["HistoryPanel.recentlyClosedCell"])
+        //XCTAssertTrue(app.tables.cells.staticTexts["Example Domain"].exists)
+
+
+        // Sign into Firefox Accounts
+        navigator.goto(BrowserTabMenu)
+        navigator.goto(FxASigninScreen)
+        waitforExistence(app.webViews.staticTexts["Sign in"], timeout: 10)
+        userState.fxaUsername = ProcessInfo.processInfo.environment["FXA_EMAIL"]!
+        userState.fxaPassword = ProcessInfo.processInfo.environment["FXA_PASSWORD"]!
+        navigator.performAction(Action.FxATypeEmail)
+        navigator.performAction(Action.FxATypePassword)
+        navigator.performAction(Action.FxATapOnSignInButton)
+        allowNotifications()
+
+        // Wait for initial sync to complete
+        navigator.nowAt(BrowserTab)
+        navigator.goto(SettingsScreen)
+        waitforExistence(app.tables.staticTexts["Sync Now"], timeout: 12)
+    }
+
     func testFxASyncBookmark () {
         // Go to a webpage, and add to bookmarks
         navigator.createNewTab()
